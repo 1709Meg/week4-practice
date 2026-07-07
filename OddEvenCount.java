b95dd8b6dad6b234a87f25acb4c55fad5f22a5c8
1709Meg <meghickey@yahoo.com>
2026-07-07 21:26:11 -0230

    Updated week 4 files

--- OddEvenCount.java
+++ OddEvenCount.java
@@ -2,14 +2,19 @@ public class OddEvenCount {
     public static void main(String[] args) {
         int[] numbers = { 4, 7, 2, 8, 10, 5 };
         int evenCount = 0;
+        int oddCount = 0;
 
         for (int i = 0; i < numbers.length; i++) {
 
             if (numbers[i] % 2 == 0) {
                 evenCount++;
+            } else {
+                oddCount++;
             }
 
         }
         System.out.println("Number of even = " + evenCount);
+        System.out.println("Number of odd = " + oddCount);
+
     }
 }
---
+++ SortArray.java
@@ -0,0 +1,19 @@
+public class SortArray {
+    public static void main(String[] args) {
+        int[] numbers = { 5, 2, 8, 1, 9 };
+
+        for (int i = 0; i < numbers.length - 1; i++) {
+            for (int j = i + 1; j < numbers.length; j++) {
+                if (numbers[i] > numbers[j]) {
+                    int temp = numbers[i];
+                    numbers[i] = numbers[j];
+                    numbers[j] = temp;
+                }
+
+            }
+        }
+        for (int i = 0; i < numbers.length; i++) {
+            System.out.println(numbers[i]);
+        }
+    }
+}
